1d663a8c0be8c5ada5711b7d3fdbf8d2d59512bb
Robert Saunders Jr <robjrtech@gmail.com>
2026-09-09 10:47:41 -0500

    created

---
+++ string_manipulation.py
@@ -0,0 +1,10 @@
+# Takes a user input
+user_input = input("Enter a sentence:")
+
+# Stores the users input in uppercase
+uppercase_user_input = user_input.upper()
+print("Your input in uppercase", uppercase_user_input)
+
+# Stores the users input in reserve
+reverse_user_input = user_input[::-1]
+print("Your input in reversed order", reverse_user_input)
