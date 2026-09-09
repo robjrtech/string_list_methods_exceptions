fec5711a32953c6de2f37a5fdbf511f7b53f5806
RSaundersJr <robjrtech@gmail.com>
2026-09-09 11:06:20 -0500

    Completed string_manipulation.py

--- string_manipulation.py
+++ string_manipulation.py
@@ -5,6 +5,19 @@ user_input = input("Enter a sentence:")
 uppercase_user_input = user_input.upper()
 print("Your input in uppercase", uppercase_user_input)
 
-# Stores the users input in reserve
-reverse_user_input = user_input[::-1]
+# Stores the users input in reverse
+reverse_user_input = " ".join(user_input.split()[::-1])
 print("Your input in reversed order", reverse_user_input)
+
+
+# Checks if the user input contains vowels and counts them
+vowels = "aeiouAEIOU"
+
+if any(char in vowels for char in user_input):
+    count = sum(1 for char in user_input if char in vowels)
+    print("Your input contains vowels")
+    print("Number of vowels:", count)
+
+# Replaces spaces in the user input with hyphens
+user_input_no_spaces = user_input.replace(" ", "-")
+print("Your input with spaces replaced by hyphens:", user_input_no_spaces)
